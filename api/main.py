d43a71cfcf9044bac14c97350f1b8531fabf0aff
Murat-HH <167295172+Murat666666@users.noreply.github.com>
2026-04-01 12:21:10 +0200

    fix: stable web UI without template

--- api/main.py
+++ api/main.py
@@ -58,4 +58,8 @@ async def scan_web(host: str = Form(...), ports: str = Form(...)):
         <br><a href="/">⬅ Back</a>
     </body>
     </html>
+HEAD
     """
+
+    """
+660491d (fix: stable web UI without template)
