9c642374cdec7789666cfddc09242396716863bc
Murat-HH <167295172+Murat666666@users.noreply.github.com>
2026-04-05 21:46:53 +0200

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,10 +1,8 @@
 import sqlite3
 import json
 import os
-from datetime import datetime
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB = os.path.join(BASE_DIR, "scans.db")
+DB = "scans.db"
 
 
 def init_db():
@@ -16,8 +14,7 @@ def init_db():
         id INTEGER PRIMARY KEY,
         host TEXT,
         ports TEXT,
-        result TEXT,
-        created_at TEXT
+        result TEXT
     )
     """)
 
@@ -29,31 +26,10 @@ def save_scan(host, ports, result):
     conn = sqlite3.connect(DB)
     c = conn.cursor()
 
-    c.execute("""
-    INSERT INTO scans (host, ports, result, created_at)
-    VALUES (?, ?, ?, ?)
-    """, (host, ports, json.dumps(result), datetime.now().isoformat()))
+    c.execute(
+        "INSERT INTO scans (host, ports, result) VALUES (?, ?, ?)",
+        (host, ports, json.dumps(result))
+    )
 
     conn.commit()
     conn.close()
-
-
-def get_scans():
-    conn = sqlite3.connect(DB)
-    c = conn.cursor()
-
-    c.execute("SELECT * FROM scans ORDER BY id DESC")
-    rows = c.fetchall()
-
-    conn.close()
-
-    return [
-        {
-            "id": r[0],
-            "host": r[1],
-            "ports": r[2],
-            "result": json.loads(r[3]),
-            "created_at": r[4]
-        }
-        for r in rows
-    ]
